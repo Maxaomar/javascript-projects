2285495f1ea7bc7d550360ff20f1dba5f841a31c
MOHAMED <ahmadiin612@gmail.com>
2025-02-21 21:11:54 +0300

    loops

--- amazon-website/utils/darwiish.js
+++ amazon-website/utils/darwiish.js
@@ -1,9 +1,23 @@
 import { products } from "../data/products.js";
+const root = document.getElementById("root")
+
 
 
 let productsHTML;
 
 products.forEach((product) => {
-    
+    productsHTML += `
+    <div>
+        <div class="product-image">${product.image}</div>
+        <div class="product-name">${product.name}</div>
+        <div class="product-ratings">
+            <div>${product.rating.stars}</div>
+            <div>${product.rating.count}</div>
+        </div>
+        <div class="product-price">${product.priceCents}</div>
+     </div>
+    `
+    root.innerHTML = productsHTML;
 })
+
 
